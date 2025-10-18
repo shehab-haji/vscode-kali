6efa3d0bb59b4006ae3afe847ec323c07bd0645f
shehab-haji <chiheb.haji@gmail.com>
2025-10-19 02:02:52 +0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ sudo apt update && sudo apt upgrade -y
 sudo install -d -m 0755 /etc/apt/keyrings
 curl -fsSL -o microsoft-rolling.asc https://packages.microsoft.com/keys/microsoft.asc
 sudo gpg --dearmor -o /etc/apt/keyrings/microsoft.gpg microsoft-rolling.asc
-echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/code stable main' \
+echo 'deb [signed-by=/etc/apt/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/code stable main' \
  | sudo tee /etc/apt/sources.list.d/vscode.list
 sudo apt update
 sudo apt install -y code
